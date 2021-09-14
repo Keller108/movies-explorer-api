1dca8f22623a4137605c69b93691e5c7c73afb86
Nikita Isaev <keller108@yandex.ru>
2021-09-14 11:11:56 +0500

    Обновлены корсы

--- app.js
+++ app.js
@@ -9,8 +9,35 @@ const rateLimiter = require('./utils/rateLimiter');
 const { DB, PORT } = require('./utils/config');
 require('dotenv').config();
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://localhost:3000',
+  'http://localhost:3003',
+  'http://movies108.nomoredomains.monster',
+  'https://movies108.nomoredomains.monster',
+];
+
 const app = express();
 app.use(helmet());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
